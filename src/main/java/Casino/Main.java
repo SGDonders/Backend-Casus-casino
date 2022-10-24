6551abef6a7edc33b230be53e6cf3f4f44bad6f6
SGDonders <donders_sg@live.com>
2022-10-24 13:14:53 +0200

    first commit

---
+++ src/main/java/Casino/Main.java
@@ -0,0 +1,14 @@
+package Casino;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+        HigherLowerGame game = new HigherLowerGame(inputScanner);
+
+        game.playGame();
+    }
+}
